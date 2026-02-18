c8f72847806786bfacb3a0b9edae6e7bd046d946
aditiverma-glitch <20aditi.verma@gmail.com>
2026-02-18 16:11:16 +0530

    Added Changes

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,4 +1,28 @@
-function Counter() {
+import React, {useState} from "react";
 
+function Counter() {
+    const [count,setCount] = useState(0);
+
+    function increment() {
+        setCount(c=> c+1);
+    }
+    function decrement() {
+        setCount(c=> c-1);
+    }
+    function reset() {
+        setCount(0);
+    }
+
+    /* const increment= () => {
+        setCount(count+1);
+    }*/
+
+    return(
+        <div className="Counter"><b>Count: {count}</b> <br />
+        <button className="counterbutton" onClick={decrement}>Decrement</button>
+        <button className="counterbutton" onClick={reset}>Reset</button>
+        <button className="counterbutton" onClick={increment}>Increment</button></div>
+    );
 }
+
 export default Counter;
